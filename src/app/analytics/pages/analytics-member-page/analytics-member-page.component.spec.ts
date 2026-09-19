1f62bb4c84deb44800de67b11be7f5ecd2cdddc0
SebastianPacheco <u202110291@upc.edu.pe>
2026-09-19 11:48:58 -0500

    refactor: migrate analytics deprecated code to modern conditionals.

--- src/app/analytics/pages/analytics-member-page/analytics-member-page.component.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AnalyticsMemberPageComponent } from './analytics-member-page.component';
-
-describe('AnalyticsMemberPageComponent', () => {
-  it('should create', () => {
-    const component = new AnalyticsMemberPageComponent();
-    expect(component).toBeTruthy();
-  });
-});
